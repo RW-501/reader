9036ae799a9851a61ccafd02eae8c4d00ffe99ff
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-07-28 14:17:49 -0500

    Update script.js

--- Editor/script.js
+++ Editor/script.js
@@ -1,10 +1,3 @@
-// Function to get documents from local storage
-function getDocumentsFromLocalStorage() {
-  const documentsJSON = localStorage.getItem('documents');
-  return documentsJSON ? JSON.parse(documentsJSON) : [];
-}
-
-
 // Function to save the document to local storage
 function saveDocumentToLocalStorage() {
   const title = prompt('Enter the title for the document:');
@@ -74,40 +67,3 @@ openDocumentButton.addEventListener('click', showDocumentSelectionPopup);
 // Event listener to handle closing the document selection pop-up
 const closePopupButton = document.getElementById('closePopupButton');
 closePopupButton.addEventListener('click', hideDocumentSelectionPopup);
-
-
-
-
-
-// Function to show the document selection pop-up
-function showDocumentSelectionPopup() {
-  const documentSelectionPopup = document.getElementById('documentSelectionPopup');
-  documentSelectionPopup.style.display = 'block';
-
-  // Populate the document list from local storage
-  const documentList = document.getElementById('documentList');
-  documentList.innerHTML = '';
-  const documents = getDocumentsFromLocalStorage();
-  documents.forEach((document) => {
-    const listItem = document.createElement('li');
-    listItem.textContent = document.title;
-    listItem.addEventListener('click', () => {
-      selectDocument(document);
-      hideDocumentSelectionPopup();
-    });
-    documentList.appendChild(listItem);
-  });
-}
-
-// Function to hide the document selection pop-up
-function hideDocumentSelectionPopup() {
-  const documentSelectionPopup = document.getElementById('documentSelectionPopup');
-  documentSelectionPopup.style.display = 'none';
-}
-
-// Function to select a document and populate the editor with its content
-function selectDocument(document) {
-  const editorTextElement = document.getElementById('editorText');
-  editorTextElement.innerHTML = document.content;
-}
-
